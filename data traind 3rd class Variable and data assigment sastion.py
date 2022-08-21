94d9ccd13eec47dfa315071ddf31c56151602d5e
abhayshrivas <110330324+abhayshrivas@users.noreply.github.com>
2022-08-22 01:16:30 +0530

    Add files via upload

---
+++ data traind 3rd class Variable and data assigment sastion.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+# Variable and data assigment
+# a = variable 
+# 20 = data
+a = 20
+
+
+# In[35]:
+
+
+a
+
+
+# In[36]:
+
+
+print (a)
+
+
+# In[37]:
+
+
+type (a)
+
+
+# In[38]:
+
+
+b = 20.0
+
+
+# In[39]:
+
+
+a = 10
+b = 20
+
+
+# In[40]:
+
+
+c = a + b
+
+
+# In[42]:
+
+
+print (c )
+
+
+# In[43]:
+
+
+print (c * 10)
+
+
+# In[44]:
+
+
+c = 6+2j
+
+type(c)
+
+
+# In[48]:
+
+
+print ("hello world")
+
+
+# In[49]:
+
+
+first_string = 'Python'
+second_string = 'is a powerful'
+third_string = 'Language'
+
+
+# In[50]:
+
+
+type(first_string)
+
+
+# In[51]:
+
+
+print(first_string+second_string+third_string)
+
+
+# In[59]:
+
+
+print ( 'wow '+ 'python is '+ 'Easy')
+
+
+# In[58]:
+
+
+print ( ' Day ' * 5 )
+
+
+# In[60]:
+
+
+print('-' * 200)
+
+
+# In[61]:
+
+
+print(' Day ' + 2  )
+
+
+# In[62]:
+
+
+print ( ' Day ' + str(2) )
+
+
+# In[ ]:
+
+
+
+
---
+++ data traind 3rd day class python keyword sasation.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[31]:
+
+
+import keyword
+
+
+# In[32]:
+
+
+print (keyword.kwlist)
+
+
+# In[33]:
+
+
+True and False
+
+
+# In[34]:
+
+
+True or False
+
+
+# In[38]:
+
+
+not True
+
+
+# In[40]:
+
+
+True is True
+
+
+# In[41]:
+
+
+True is False
+
+
+# In[42]:
+
+
+'b' is 'c'
+
+
+# In[43]:
+
+
+10 is 11
+
+
+# In[44]:
+
+
+True + True
+
+
+# In[45]:
+
+
+True + False
+
+
+# In[46]:
+
+
+False + False
+
+
+# In[47]:
+
+
+True - False
+
+
+# In[48]:
+
+
+8 == 9
+
+
+# In[49]:
+
+
+8 is 9
+
+
+# In[50]:
+
+
+10 is 10
+
+
+# In[51]:
+
+
+True and False or True
+
+
+# In[ ]:
+
+
+
+
